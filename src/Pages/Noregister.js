e99fe7d73824b87a2cd0677718ec311906b3a208
rolando <rolando22_@outlook.com>
2021-07-24 09:33:09 -0500

    nuevo 2

--- src/Pages/Noregister.js
+++ src/Pages/Noregister.js
@@ -11,7 +11,7 @@ export const Noregister = () => (
             return(
                 <Fragment>
                     <Registermutation>
-
+                        
                         //autentication con graphql
                         {
                             (register) => {
